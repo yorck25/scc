8231f7fb1a4634bacdeea83914c498c675a13e64
yorck25 <100796096+yorck25@users.noreply.github.com>
2025-04-05 00:15:23 +0200

    * adds dummy house obj

--- SCC/Assets/GirdSystem.cs
+++ SCC/Assets/GirdSystem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GridSystem : MonoBehaviour
 {
     public GameObject objectToPlace;
     public bool isBuildModeEnabled = false;
