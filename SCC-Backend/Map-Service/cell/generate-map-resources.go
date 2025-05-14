1ec39f2cf9041d5c466550156d1a1aca963006ec
yorck25 <100796096+yorck25@users.noreply.github.com>
2025-05-14 21:57:55 +0200

    * adds that random cell block get selected for the cluster

--- SCC-Backend/Map-Service/cell/generate-map-resources.go
+++ SCC-Backend/Map-Service/cell/generate-map-resources.go
@@ -3,6 +3,7 @@ package cell
 import (
 	"fmt"
 	"map-service/core"
+	"math"
 	"math/rand/v2"
 	"strconv"
 )
@@ -44,11 +45,75 @@ func TestCreateCluster(ctx *core.WebContext) error {
 		}
 	}
 
-	GenerateRandomFieldCluster(x, y, cells)
-	return ctx.Success(cells)
+	newCells, err := GenerateRandomFieldCluster(x, y, cells)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Success(newCells)
 }
 
-func GenerateRandomFieldCluster(x int, y int, cells []Cell) {
-	fmt.Print(rand.IntN(x))
-	fmt.Print(rand.IntN(y))
+func GenerateRandomFieldCluster(x int, y int, cells []Cell) ([]Cell, error) {
+	var percentage = 0.25
+	minClusterRadius := 1
+	//spreadChance := 0.6
+
+	xBuffer := int(math.Round(float64(x) * percentage))
+	yBuffer := int(math.Round(float64(y) * percentage))
+
+	setSeedOfCluster := func(coordinate Coordinate) {
+		for i := range cells {
+			if cells[i].X == coordinate.X && cells[i].Y == coordinate.Y {
+				cells[i].AvailableGoods = 1
+			}
+		}
+	}
+
+	//defines the cluster origin seed
+	xSeed := rand.IntN((x-xBuffer)-xBuffer) + xBuffer
+	ySeed := rand.IntN((y-yBuffer)-yBuffer) + yBuffer
+
+	originCoordinate := Coordinate{
+		X: xSeed,
+		Y: ySeed,
+	}
+
+	setSeedOfCluster(originCoordinate)
+
+	var selectedCoordinates []Coordinate
+
+	xMin := rand.IntN((x-xBuffer)-minClusterRadius) + minClusterRadius
+	xMax := rand.IntN((x-xBuffer)-minClusterRadius) + minClusterRadius
+
+	yMin := rand.IntN((y-yBuffer)-minClusterRadius) + minClusterRadius
+	yMax := rand.IntN((y-yBuffer)-minClusterRadius) + minClusterRadius
+
+	xMin = (originCoordinate.X - xMin) + 1
+	xMax = (originCoordinate.X + xMax)
+
+	yMin = (originCoordinate.Y - yMin) + 1
+	yMax = (originCoordinate.Y + yMax)
+
+	//fmt.Printf("xMin=%d, xMax=%d | yMin=%d, yMax=%d\n", xMin, xMax, yMin, yMax)
+
+	for i := xMin; i < xMax; i++ {
+		for j := yMin; j < yMax; j++ {
+			newCoordinate := Coordinate{
+				X: i,
+				Y: j,
+			}
+
+			if newCoordinate != originCoordinate {
+				selectedCoordinates = append(selectedCoordinates, newCoordinate)
+			} else {
+				fmt.Println("skipping origin coordinate")
+			}
+		}
+	}
+
+	for _, coordinate := range selectedCoordinates {
+		setSeedOfCluster(coordinate)
+	}
+
+	return cells, nil
 }
